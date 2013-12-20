16bbe504dac48c00234be93993f3d3df897266b8
adamjermyn <adamjermyn@gmail.com>
2013-12-20 18:08:42 -0500

    Delete hubbardCheck.py

--- Libraries/hubbardCheck.py
+++
@@ -1,8 +0,0 @@
-from analysis import *
-
-def measure(data):
-	return np.log(np.sqrt((data[0]-data[1])**2+(data[0]-data[2])**2+(data[1]-data[2])**2))
-
-data=[measure(eigenPotts(3+n,0.005,0,3)) for n in range(6)]
-data=np.array([i-data[0] for i in data])
-print data
